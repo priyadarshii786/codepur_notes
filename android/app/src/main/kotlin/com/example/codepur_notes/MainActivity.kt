8e46d7e74e8032bd2e29a4511130b9cd78dcf2e6
priyadarshii786 <tathagatp918@gmail.com>
2023-10-16 01:36:55 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/codepur_notes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.codepur_notes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final int days = 30;
+  final String name = "Codepur";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+            "Catalog App"), //title ek widget leta hai(ie; text), agar string leta toh simply string pass kr dete
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days days of flutter by $name"),
+        ),
+      ),
+      drawer: const Drawer(),
+    );
+  }
+}
