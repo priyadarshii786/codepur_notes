62f5d67e8b06d7e539ca691a991ad7dfad4ccd42
priyadarshii786 <tathagatp918@gmail.com>
2023-10-16 18:12:44 +0530

    day 3 complete

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: HomePage(),
+      /* materialApp ke andar hi "home" define kiye hai jo ki HomePage() class leta hai........thenafter "themeMode" define kiye hai jisko ki "ThemeMode.dark" diye hai......ab dark theme ko apply krwaane ke liye sirf itna krna hi kaafi nahi hai, uske liye hi ab "darkTheme" define krna pd raha hai, jo ki "ThemeData" class leta hai and uske andar ek property hota hai brightness, jisko ki "Brightness.dark" rkh rhe hai......now ab "ThemeMode.dark" ke jagah "ThemeMode.light" change krenge toh voh v work karega  */
+      themeMode: ThemeMode.light,
+      darkTheme: ThemeData(brightness: Brightness.dark),
+
+      /* ab agar light theme ka property dena hoga toh below written way se denge......."primaryColor" se v color change kr skte hai yaha but not using it here and yaha "primarySwatch" isliye use kr rhe because we want jitne v colors hai vo material ke hisaab se khud ko adjust kr le */
+
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      /* "primarySwatch" me ek "colors" naam ka library hai material ke andar jiska ki use kiye hai yaha ie; colors.{desired color} */
+
+      /*--------------------------------------------------- --------------------------------------------------- 
+      
+      Now we will learn about "routes"
+      
+      */
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -19,3 +19,40 @@ class HomePage extends StatelessWidget {
     );
   }
 }
+
+// class ke andar function banate hai toh "methods" bolte hai usko, and class ke baahar funciton bnate ahi toh "function" hi bolte hai.
+
+
+
+/* 
+
+bringVegetables({int rupees = 100}){    
+  
+  -------- -> yaha pr iss tarah se curly brackett ke andar rupees define kiye hai...if "bringVegetables" call krte samay(ie; bringVegetables(); ) rupees ka value pass nahi krte hai toh, by default voh rupees ka value 100 le lega......or agar iss type se call krenge(ie; bringVegetables(rupees: 50); ) then rupees ka value 50 hi lega.
+
+}
+
+-----------------------------------------------------------
+-----------------------------------------------------------
+
+bringVegetables(rupees: 50)
+
+bringVegetables({bool thaila, int rupees = 100}){
+ 
+ --------- -> upar me call krte samay thaila ko define kr v skte hai ya nahi v.....but below written example makes it mandatory to define thaila while calling it.
+
+  }
+
+-----------------------------------------------------------
+-----------------------------------------------------------
+
+  bringVegetables(thaila: false, rupees: 50)
+
+  bringVegetables({@required bool thaila, int rupees = 100}){
+ 
+ --------- -> function call krte waqt thaila define krna isliye jaruri ho gya because we used "@required" while making the function. 
+
+  }
+
+
+*/
